b3b38c42dc89ce3a90470305bcc8009a8c2dfe9f
Jordan Lei <thunderflash.lei@gmail.com>
2020-02-18 21:14:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,36 +9,36 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-# @app.route('/get_traffic')
-# def get_traffic():
-#     df = generate_calendar_matrix(traffic_data())
-#     return df.to_json(orient='columns')
+@app.route('/get_traffic')
+def get_traffic():
+    df = generate_calendar_matrix(traffic_data())
+    return df.to_json(orient='columns')
 
-# @app.route('/get_events')
-# def get_events():
-#     df = generate_calendar_matrix(events_data())
-#     return df.to_json(orient='columns') 
+@app.route('/get_events')
+def get_events():
+    df = generate_calendar_matrix(events_data())
+    return df.to_json(orient='columns') 
 
-# @app.route('/test_post', methods=['POST'])
-# def test_user():
-#     if request.method == 'POST':
-#         user = request.json
-#         weights = {'traffic': 1, 
-#                   'events' : 1}
+@app.route('/test_post', methods=['POST'])
+def test_user():
+    if request.method == 'POST':
+        user = request.json
+        weights = {'traffic': 1, 
+                  'events' : 1}
 
-#         weights['traffic'] = (-1 * (user['extentFrustratedInTraffic'] - 3))/2
-#         mask = np.ones(CALENDAR_SIZE)
-#         mask[:user['earliestHourWillingToWork']] = 0
-#         mask[user['latestHourWillingToWork']:] = 0
-#         if not user['drivesOnWeekends']:
-#             mask[:, 5:] = 0
-#         if not user['drivesOnWeekdays']:
-#             mask[:, :5] = 0
-#         mask = mask.astype(int)
-#         model_output = simple_model(weights, mask, hours = user['hoursPerWeek'], verbose= False)
-#         df = generate_calendar_matrix(model_output)
-#         return df.to_json(orient='columns') 
-#     return "Error"
+        weights['traffic'] = (-1 * (user['extentFrustratedInTraffic'] - 3))/2
+        mask = np.ones(CALENDAR_SIZE)
+        mask[:user['earliestHourWillingToWork']] = 0
+        mask[user['latestHourWillingToWork']:] = 0
+        if not user['drivesOnWeekends']:
+            mask[:, 5:] = 0
+        if not user['drivesOnWeekdays']:
+            mask[:, :5] = 0
+        mask = mask.astype(int)
+        model_output = simple_model(weights, mask, hours = user['hoursPerWeek'], verbose= False)
+        df = generate_calendar_matrix(model_output)
+        return df.to_json(orient='columns') 
+    return "Error"
 
 
 if __name__ == '__main__':
