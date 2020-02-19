5c9090a03825d5927101a43603709446867a45f4
Jordan Lei <thunderflash.lei@gmail.com>
2020-02-18 20:14:33 -0500

    commit

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 web: gunicorn server:app
+=======
+web:gunicorn app:server
+>>>>>>> parent of 10911fb... name changes
--- server.py
+++ server.py
@@ -7,39 +7,35 @@ CALENDAR_SIZE = (24, 7) #set calendar size to 24 by 7
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return 'Hello, World!'
 
-# @app.route('/get_traffic')
-# def get_traffic():
-#     df = generate_calendar_matrix(traffic_data())
-#     return df.to_json(orient='columns')
+@app.route('/get_traffic')
+def get_traffic():
+    df = generate_calendar_matrix(traffic_data())
+    return df.to_json(orient='columns')
 
-# @app.route('/get_events')
-# def get_events():
-#     df = generate_calendar_matrix(events_data())
-#     return df.to_json(orient='columns') 
+@app.route('/get_events')
+def get_events():
+    df = generate_calendar_matrix(events_data())
+    return df.to_json(orient='columns') 
 
-# @app.route('/test_post', methods=['POST'])
-# def test_user():
-#     if request.method == 'POST':
-#         user = request.json
-#         weights = {'traffic': 1, 
-#                   'events' : 1}
+@app.route('/test_post', methods=['POST'])
+def test_user():
+    if request.method == 'POST':
+        user = request.json
+        weights = {'traffic': 1, 
+                  'events' : 1}
 
-#         weights['traffic'] = (-1 * (user['extentFrustratedInTraffic'] - 3))/2
-#         mask = np.ones(CALENDAR_SIZE)
-#         mask[:user['earliestHourWillingToWork']] = 0
-#         mask[user['latestHourWillingToWork']:] = 0
-#         if not user['drivesOnWeekends']:
-#             mask[:, 5:] = 0
-#         if not user['drivesOnWeekdays']:
-#             mask[:, :5] = 0
-#         mask = mask.astype(int)
-#         model_output = simple_model(weights, mask, hours = user['hoursPerWeek'], verbose= False)
-#         df = generate_calendar_matrix(model_output)
-#         return df.to_json(orient='columns') 
-#     return "Error"
-
-
-if __name__ == '__main__':
-    app.run()
+        weights['traffic'] = (-1 * (user['extentFrustratedInTraffic'] - 3))/2
+        mask = np.ones(CALENDAR_SIZE)
+        mask[:user['earliestHourWillingToWork']] = 0
+        mask[user['latestHourWillingToWork']:] = 0
+        if not user['drivesOnWeekends']:
+            mask[:, 5:] = 0
+        if not user['drivesOnWeekdays']:
+            mask[:, :5] = 0
+        mask = mask.astype(int)
+        model_output = simple_model(weights, mask, hours = user['hoursPerWeek'], verbose= False)
+        df = generate_calendar_matrix(model_output)
+        return df.to_json(orient='columns') 
+    return "Error"
